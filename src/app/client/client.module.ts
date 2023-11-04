10fe7be7414b2a352cd5aacd13eac079b87a81a5
KillianWllm <killianwilliame38@gmail.com>
2023-11-04 17:24:09 +0100

    V1.0

---
+++ src/app/client/client.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { FormulaireAjoutComponent } from './adresses-livraison/formulaire-ajout/formulaire-ajout.component';
+import { ListeAdressesComponent } from './adresses-livraison/adresses/adresses.component';
+import { ClientInformationComponent } from './client-information/client-information.component';
+import { ListeCommandesComponent } from './liste-commandes/liste-commandes.component';
+import { DetailsOrderComponent } from './detail-commande/detail-commandes.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [
+    FormulaireAjoutComponent,
+    ListeAdressesComponent,
+    ClientInformationComponent,
+    ListeCommandesComponent,
+    DetailsOrderComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule
+  ],
+  exports: [
+    FormulaireAjoutComponent
+  ]
+})
+export class ClientModule { }
